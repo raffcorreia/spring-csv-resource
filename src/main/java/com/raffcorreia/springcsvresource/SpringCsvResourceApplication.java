bcbc9d6b03d4a2a868e2c9ef8dd08f6ad144b70e
Rafael Correia <raffcorreia@gmail.com>
2021-05-23 14:17:14 -0400

    Initial commit, generated files

---
+++ src/main/java/com/raffcorreia/springcsvresource/SpringCsvResourceApplication.java
@@ -0,0 +1,13 @@
+package com.raffcorreia.springcsvresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCsvResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCsvResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raffcorreia/springcsvresource/SpringCsvResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raffcorreia.springcsvresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCsvResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
